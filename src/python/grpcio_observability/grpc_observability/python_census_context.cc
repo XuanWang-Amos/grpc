511ff81c22bbab1b5ba67cf6e9658c0488f98b35
Xuan Wang <xuanwn@google.com>
2023-06-16 22:41:58 +0000

    Resample trace option if parent_context exist

--- src/python/grpcio_observability/grpc_observability/python_census_context.cc
+++ src/python/grpcio_observability/grpc_observability/python_census_context.cc
@@ -223,6 +223,9 @@ Span Span::StartSpan(absl::string_view name,
   std::string parent_span_id = parent_context.SpanId();
   std::string span_id = GenerateSpanId();
   bool should_sample = parent_context.IsSampled();
+  if (!should_sample) {
+    should_sample = ShouldSample(std::string(trace_id));
+  }
   auto start_time = absl::Now();
   SpanContext context(trace_id, span_id, should_sample);
   return Span(std::string(name), parent_span_id, start_time, context);
