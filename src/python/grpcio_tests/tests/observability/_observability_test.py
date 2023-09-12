4ceba5bf251eb4af0e7d1d337fad77798296690d
Xuan Wang <xuanwn@google.com>
2023-09-12 21:03:56 +0000

    Fail O11y test

--- src/python/grpcio_tests/tests/observability/_observability_test.py
+++ src/python/grpcio_tests/tests/observability/_observability_test.py
@@ -174,6 +174,7 @@ class ObservabilityTest(unittest.TestCase):
             self._start_server()
             unary_unary_call(port=self._port)
 
+        self.assertEqual(2, 3)
         self.assertGreater(len(self.all_metric), 0)
         self.assertGreater(len(self.all_span), 0)
         self._validate_metrics(self.all_metric)
