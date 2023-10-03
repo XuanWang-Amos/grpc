c0d230bf0d55deacaa25a1ab47d7cff5146b6237
Xuan Wang <xuanwn@google.com>
2023-10-03 02:42:46 +0000

    Fix issue in args

--- tools/run_tests/helper_scripts/run_tests_in_workspace.sh
+++ tools/run_tests/helper_scripts/run_tests_in_workspace.sh
@@ -31,4 +31,4 @@ git submodule foreach 'cd "${repo_root}/${WORKSPACE_NAME}" \
     && git submodule update --init --reference ${repo_root}/${name} ${name}'
 
 echo "Running run_tests.py in workspace ${WORKSPACE_NAME}"
-python3 "${WORKSPACE_NAME}/tools/run_tests/run_tests.py" "-j 8 -l python -c opt --iomgr_platform native --max_time 5400"
+python3 "${WORKSPACE_NAME}/tools/run_tests/run_tests.py -j 8 -l python -c opt --iomgr_platform native --max_time 5400"
