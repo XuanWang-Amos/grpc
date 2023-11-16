010349e6d37786082be4278e9b4eeb95a0f97734
XuanWang-Amos <XuanWang-Amos@users.noreply.github.com>
2023-11-16 22:20:59 +0000

    Automated change: Fix sanity tests

--- src/python/grpcio/grpc/_interceptor.py
+++ src/python/grpcio/grpc/_interceptor.py
@@ -694,7 +694,7 @@ class _Channel(grpc.Channel):
     ) -> grpc.UnaryUnaryMultiCallable:
         thunk = lambda m: self._channel.unary_unary(
             m, request_serializer, response_deserializer, registered_method
-        ) # pytype: disable=wrong-arg-count
+        )  # pytype: disable=wrong-arg-count
         if isinstance(self._interceptor, grpc.UnaryUnaryClientInterceptor):
             return _UnaryUnaryMultiCallable(thunk, method, self._interceptor)
         else:
@@ -710,7 +710,7 @@ class _Channel(grpc.Channel):
     ) -> grpc.UnaryStreamMultiCallable:
         thunk = lambda m: self._channel.unary_stream(
             m, request_serializer, response_deserializer, registered_method
-        ) # pytype: disable=wrong-arg-count
+        )  # pytype: disable=wrong-arg-count
         if isinstance(self._interceptor, grpc.UnaryStreamClientInterceptor):
             return _UnaryStreamMultiCallable(thunk, method, self._interceptor)
         else:
@@ -726,7 +726,7 @@ class _Channel(grpc.Channel):
     ) -> grpc.StreamUnaryMultiCallable:
         thunk = lambda m: self._channel.stream_unary(
             m, request_serializer, response_deserializer, registered_method
-        ) # pytype: disable=wrong-arg-count
+        )  # pytype: disable=wrong-arg-count
         if isinstance(self._interceptor, grpc.StreamUnaryClientInterceptor):
             return _StreamUnaryMultiCallable(thunk, method, self._interceptor)
         else:
@@ -742,7 +742,7 @@ class _Channel(grpc.Channel):
     ) -> grpc.StreamStreamMultiCallable:
         thunk = lambda m: self._channel.stream_stream(
             m, request_serializer, response_deserializer, registered_method
-        ) # pytype: disable=wrong-arg-count
+        )  # pytype: disable=wrong-arg-count
         if isinstance(self._interceptor, grpc.StreamStreamClientInterceptor):
             return _StreamStreamMultiCallable(thunk, method, self._interceptor)
         else:
