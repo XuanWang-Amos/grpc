05522686b295fa48c3ac8944fe0324158e61f5b2
Xuan Wang <xuanwn@google.com>
2025-01-28 01:04:59 +0000

    Try use GRPC_PYTHON_BUILD_WITH_STATIC_LIBSTDCXX flag

--- tools/run_tests/artifacts/artifact_targets.py
+++ tools/run_tests/artifacts/artifact_targets.py
@@ -200,6 +200,7 @@ class PythonArtifact:
             )
             environ["PIP"] = "/opt/python/{}/bin/pip".format(self.py_version)
             environ["GRPC_SKIP_PIP_CYTHON_UPGRADE"] = "TRUE"
+            environ["GRPC_PYTHON_BUILD_WITH_STATIC_LIBSTDCXX"] = "TRUE"
 
             if self.arch in ("x86", "aarch64"):
                 environ["GRPC_SKIP_TWINE_CHECK"] = "TRUE"
@@ -212,7 +213,6 @@ class PythonArtifact:
                 environ["GRPC_BUILD_GRPCIO_TOOLS_DEPENDENTS"] = "TRUE"
             else:
                 environ["GRPC_RUN_AUDITWHEEL_REPAIR"] = "TRUE"
-                environ["GRPC_PYTHON_BUILD_WITH_STATIC_LIBSTDCXX"] = "TRUE"
 
             return create_docker_jobspec(
                 self.name,
